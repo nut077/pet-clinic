248ba113ac42cd85dba11316c94073d215e4e91e
freedom <nuttapon@csc.co.th>
2018-09-27 16:57:49 +0700

    add mockmvc ownercontroller test

--- pet-clinic-data/src/main/java/com/github/nut077/petclinic/entity/Owner.java
+++ pet-clinic-data/src/main/java/com/github/nut077/petclinic/entity/Owner.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @NoArgsConstructor
@@ -16,7 +17,7 @@ public class Owner extends Person {
     private String city;
     private String telephone;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
-    private Set<Pet> pets;
+    private Set<Pet> pets = new LinkedHashSet<>();
 
     @Builder
     public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
---
+++ pet-clinic-web/src/test/java/com/github/nut077/petclinic/controller/OwnerControllerTest.java
@@ -0,0 +1,57 @@
+package com.github.nut077.petclinic.controller;
+
+import com.github.nut077.petclinic.entity.Owner;
+import com.github.nut077.petclinic.service.jpa.OwnerJpaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    private OwnerJpaService service;
+
+    @InjectMocks
+    private OwnerController controller;
+
+    private MockMvc mockMvc;
+
+    private List<Owner> owners;
+
+
+    @BeforeEach
+    void setUp() {
+        owners = new ArrayList<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void ownerList() throws Exception {
+        when(service.findAll()).thenReturn(owners);
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attributeExists("owners"))
+                .andExpect(model().attribute("owners", hasSize(2)))
+                .andReturn();
+
+        verifyZeroInteractions(service);
+    }
+}
